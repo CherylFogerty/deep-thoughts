53bb4653daa3cfebe2776468305136fe1f3410a5
Cheryl Fogerty <cfogerty@aol.com>
2021-05-18 16:14:53 -0500

    add routes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,8 @@
-import gql from 'graphql-tag';
+    import gql from 'graphql-tag';
 
-export const QUERY_THOUGHTS = gql`
-query thoughts($username: String) {
-    thoughts(username: $username) {
+    export const QUERY_THOUGHTS = gql`
+    query thoughts($username: String) {
+        thoughts(username: $username) {
         _id
         thoughtText
         createdAt
@@ -14,6 +14,45 @@ query thoughts($username: String) {
             username
             reactionBody
         }
+        }
     }
-}
-`;
+    `;
+
+    export const QUERY_THOUGHT = gql`
+    query thought($id: ID!) {
+        thought(_id: $id) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+        reactions {
+            _id
+            createdAt
+            username
+            reactionBody
+        }
+        }
+    }
+    `;
+
+    export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+        _id
+        username
+        email
+        friendCount
+        friends {
+            _id
+            username
+        }
+        thoughts {
+            _id
+            thoughtText
+            createdAt
+            reactionCount
+        }
+        }
+    }
+    `;
